fcc58fb3210ba49077ecb0277d4ec3767586838a
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-02 22:59:39 +0000

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+// Run this command in your terminal to install tailwindcss and its corresponding type declarations
+// npm install tailwindcss @types/tailwindcss --save-dev
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
