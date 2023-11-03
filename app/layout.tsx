95cce9995aaa96215c1596cfeb48308e15f2d05b
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-03 20:10:04 +0000

    Layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative bg-[#e4d4c8]`}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
