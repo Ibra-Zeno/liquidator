351a4d8cf0fd67f40d0f3d9c288195fe8cef8e61
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-03 00:13:36 +0000

    Fake logo

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface LogoProps {}
+
+const Logo: FC<LogoProps> = ({}) => {
+  return <div className="font-xl p-1 font-bold">Logo</div>;
+};
+
+export default Logo;
