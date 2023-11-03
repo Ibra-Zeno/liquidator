95e1daecb3bd62b3e0dc479c491870c9d86b02cb
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-03 23:41:37 +0000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { ChevronRight, Users2, CheckSquare, LockKeyhole } from "lucide-react";
 import Button from "@/components/ui/index/button";
-
-const logoCloud = [
-  {
-    name: "Malaysia Department of Insolvency (MDI)",
-    href: "http://www.insolvensi.gov.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Companies Commission of Malaysia (SSM)",
-    href: "http://www.ssm.com.my/en",
-    imageSrc: "",
-  },
-  {
-    name: "The Malaysian Bar",
-    href: "http://www.malaysianbar.org.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Myeg Services Berhad",
-    href: "http://www.myeg.com.my/",
-    imageSrc: "",
-  },
-  {
-    name: "The Board of Valuers, Appraisers, Estate Agents and Property Managers Malaysia (BOVAEP)",
-    href: "http://www.lppeh.gov.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Department of Surveys and Mapping Malaysia (JUPEM)",
-    href: "http://www.jupem.gov.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Department of Director General of Lands and Mines (JKPTG)",
-    href: "http://www.kptg.gov.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Real Estate and Housing Developers' Association Malaysia (REHDA)",
-    href: "http://www.rehda.com/",
-    imageSrc: "",
-  },
-  {
-    name: "Ministry of Housing and Local Government (KPKT)",
-    href: "http://www.kpkt.gov.my/",
-    imageSrc: "",
-  },
-  {
-    name: "Malaysia Institute of Accountants (MIA)",
-    href: "http://www.mia.org.my/",
-    imageSrc: "",
-  },
-];
+import LogoCloud from "@/components/ui/index/LogoCloud";
 
 const Index: React.FC = () => {
   return (
     <main className="relative bg-[#e4d4c8]">
-      <section className="relative pb-10 pt-24 sm:pb-16 sm:pt-12 lg:min-h-[1000px] lg:pb-24">
+      <section className="relative pb-10 pt-24 sm:pt-12 lg:min-h-[1000px] lg:pb-12">
         <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-xl text-center">
             <h1 className="text-4xl font-bold sm:text-6xl">
@@ -99,26 +47,7 @@ const Index: React.FC = () => {
               </div>
             </div>
           </div>
-          <div className="my-20 grid grid-cols-2 gap-4 sm:gap-y-8 md:grid-cols-4 xl:grid-cols-5">
-            {logoCloud.map((logo) => (
-              <div
-                key={logo.name}
-                className="flex flex-col items-center justify-start"
-              >
-                <Link href={logo.href} target="_blank" rel="noreferrer">
-                  {/* <img
-                    className="object-contain w-auto mx-auto h-14"
-                    src={logo.imageSrc}
-                    alt={logo.name}
-                  /> */}
-                  <div className="mx-auto h-32 w-32 rounded-lg bg-sky-950"></div>
-                  <p className="mt-3 text-center text-xs font-bold text-gray-800 ">
-                    {logo.name}
-                  </p>
-                </Link>
-              </div>
-            ))}
-          </div>
+          <LogoCloud />
         </div>
       </section>
     </main>
