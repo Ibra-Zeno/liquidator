c139f0ef572a9f0bffe4bbe5b6e0f8f71e05b70d
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-03 00:13:51 +0000

    client btn

---
+++ components/ui/index/button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const ButtonIndex: React.FC<ButtonProps> = ({ children, className }) => {
+  return (
+    <Button
+      className={`mt-4 inline-flex items-center bg-blue-600 px-6 font-semibold text-white transition-all duration-200 hover:bg-blue-700 focus:bg-blue-700 ${className}`}
+    >
+      {children}
+    </Button>
+  );
+};
+export default ButtonIndex;
