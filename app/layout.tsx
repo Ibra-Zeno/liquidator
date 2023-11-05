b707173c324349668e713e43bc0ff101f7be0889
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-05 23:27:37 +0000

    Update component imports in layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/ui/Navbar";
-import Footer from "@/components/ui/Footer";
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
