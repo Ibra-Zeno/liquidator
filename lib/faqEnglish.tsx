0a5d23453deaaacc46cb486b71a57ee166fc13b5
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-07 20:32:19 +0000

    Add FAQ data

---
+++ lib/faqEnglish.tsx
@@ -0,0 +1,279 @@
+export const English = [
+  {
+    "Insolvency Procedures and Liquidation Processes": [
+      {
+        q: "What is Insolvency and Winding-Up?",
+        a: () => (
+          <p>
+            The process where an insolvent company&apos;s assets are disposed
+            and realized to settle debts owing to creditors.
+          </p>
+        ),
+      },
+      {
+        q: "Under What Circumstances Can A Company Be Wound-Up By The Court?",
+        a: () => (
+          <>
+            <p>
+              Section 218 of the Company Act, 1965 sets out the circumstances in
+              which a company may be wound-up by the Court
+            </p>
+            <ul>
+              <li>
+                The company has resolved by special resolution that it should be
+                wound up by the court.
+              </li>
+              <li>The company is unable to pay its debts.</li>
+              <li>
+                The directors have acted in their own interests in the
+                company&apos;s affairs rather than in the interests of the
+                members collectively, or in a manner that seems unfair{" "}
+              </li>
+              <li>
+                The court believes that it is just and equitable for the company
+                to be wound up.
+              </li>
+              <li>
+                The number of members has fallen to one (not including a
+                wholly-owned subsidiary).
+              </li>
+              <li>
+                The company has not commenced business within a year of its
+                incorporation or has suspended business for an entire year.
+              </li>
+              <li>
+                The Memorandum and Articles of Association stipulate that the
+                company will expire at a set time or upon the occurrence of a
+                specified event.
+              </li>
+              <li>
+                The company is being used for illegal purposes or for activities
+                detrimental to national security or the public interest.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Who May Be Appointed as a Liquidator in a Court-Ordered Winding-Up?",
+        a: () => (
+          <p>
+            Only an approved liquidator [Private Liquidator – an individual who
+            is licensed as a liquidator] or the Official Receiver [Director
+            General of Insolvency (DGI) in the capacity of Official Receiver]
+            can be appointed as a liquidator in a court-ordered winding up.
+          </p>
+        ),
+      },
+      {
+        q: "What Is the Primary Function of a Liquidator?",
+        a: () => (
+          <>
+            <ul>
+              <li>
+                The principal role of a liquidator is to wind up the company by
+                collecting and liquidating assets at the best possible price and
+                in a manner that is most advantageous to the company.
+              </li>
+              <li>
+                Investigate claims by creditors and third parties, settle all
+                creditors&apos; claims and distribute any surplus assets to the
+                shareholders according to their respective rights.
+              </li>
+              <li>Look into the company&apos;s affairs and assets.</li>
+              <li>
+                Examine the conduct of its directors and any other related
+                parties.
+              </li>
+              <li>
+                It is the duty of the liquidator to ensure that the company is
+                completely dissolved at the end of the process.
+              </li>
+            </ul>
+            <p>
+              Once a winding up commences, directors of the company cease to
+              have any power to manage the affairs of the company and all
+              matters are taken over by the liquidator immediately upon the
+              commencement of the winding up.
+            </p>
+          </>
+        ),
+      },
+      {
+        q: "What Is Secured Creditors Or What Is Unsecured Creditors?",
+        a: () => (
+          <>
+            <strong>Secured Creditors</strong>
+            <p>
+              A lender who holds a legally enforceable claim over a
+              borrower&apos;s assets, with a liquidation value equal to or
+              greater than the amount of the loan. Secured creditors are
+              entitled to receive the proceeds from the sale of the assets
+              pledged, and in the event of bankruptcy, they must be satisfied
+              prior to any unsecured creditors.
+            </p>
+            <strong>Unsecured Creditors</strong>
+            <p>
+              Any individual who has submitted proof of their debt to the
+              liquidator.
+            </p>
+          </>
+        ),
+      },
+    ],
+    "Verification Process for Ownership of Abandoned Properties": [
+      {
+        q: "Why is verification of ownership necessary?",
+        a: () => (
+          <>
+            <p>Verification of ownership is imperative for several reasons:</p>
+            <ul>
+              <li>
+                {" "}
+                The company has previously failed to maintain an accurate
+                register of owners and their associated documents.
+              </li>
+              <li>
+                It is required to keep and maintain a current and precise
+                register of all housing accommodation purchasers until a
+                separate title is issued, as mandated by Section 22D(3) of the
+                Housing Development (Control and Licensing) Act 1966.
+              </li>
+              <li>
+                It helps to prevent fraudulent activities by any third parties
+                concerning the property.
+              </li>
+              <li>
+                It is needed to ascertain which properties still belong to the
+                company.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Why is proof of purchase or possession of property important?",
+        a: () => (
+          <p>
+            Providing proof of purchase or possession is critical to ensure that
+            there are no fraudulent claims made by third parties regarding the
+            property and to maintain accurate records of ownership.
+          </p>
+        ),
+      },
+      {
+        q: "What are the consequences of failing to provide proof of purchase or possession?",
+        a: () => (
+          <p>
+            Should there be a failure to present proof of purchase or
+            possession, the Liquidator will apply to the court for directions
+            and to obtain a vesting order pertaining to the said properties. Any
+            costs incurred during this process will be the responsibility of the
+            party in default.
+          </p>
+        ),
+      },
+      {
+        q: "What documents are required to prove purchase or possession of property?",
+        a: () => (
+          <>
+            <p>The following documents are required</p>
+            <ul>
+              <li>
+                Certified true copies of the principal Sale & Purchase Agreement
+                and the Deed of Assignment (if applicable), including any
+                subsequent agreements and other documentation between the first
+                purchaser and any subsequent purchasers.
+              </li>
+              <li>
+                Certified true copies of the Deed of Assignment (if applicable)
+                to show that the unit is assigned to you, or receipts to
+                demonstrate that the purchase price has been fully paid if you
+                are a cash buyer.
+              </li>
+              <li>
+                Certified true copies of the Bank Loan Agreement cum Assignment,
+                bank statements (if a loan was taken), and the Deed of Receipt
+                and Reassignment (if the loan is fully settled).
+              </li>
+              <li>
+                A copy of the most recent Council Tax Assessment Receipts.
+              </li>
+              <li>
+                Copies of the latest maintenance service charge receipts for the
+                past three months.
+              </li>
+              <li>
+                A copy of the Strata Title where the purchaser&apos;s interest
+                is registered.
+              </li>
+              <li>
+                A copy of the National Registration Identity Card (NRIC) of the
+                purchaser(s).
+              </li>
+              <li>
+                Any other relevant documents that the Liquidator may require.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+    ],
+    "Application for Transfer of Ownership": [
+      {
+        q: "Why is it important to perfect the transfer of ownership?",
+        a: () => (
+          <>
+            <p>
+              The perfection of transfer is essential to legally substantiate
+              ownership for various transactions, such as transfers, charges,
+              leases, and distribution of small estates.
+            </p>
+            <p>
+              Under Section 40A of the Strata Titles (Amendment) Act 2007,
+              purchasers are required to execute transfer and charge documents
+              within twelve months upon notification of strata title issuance by
+              the original proprietor. Failure to do so constitutes an offence,
+              incurring a fine ranging from a minimum of One Thousand Ringgit
+              Malaysia (RM1,000) to a maximum of Ten Thousand Ringgit Malaysia
+              (RM10,000) per parcel.
+            </p>
+          </>
+        ),
+      },
+      {
+        q: "What should the owner/buyer do upon receiving a notice?",
+        a: () => (
+          <>
+            <p>
+              Upon receipt of notice, the owner/buyer should engage a solicitor
+              and provide a letter of instruction to the Liquidator to initiate
+              the perfection of the transfer process.
+            </p>
+            <p>
+              The Liquidator will then sign the Form 14A and/or Consent to
+              Transfer Form, contingent upon the submission of relevant
+              documents and the payment of associated administrative costs by
+              the owner/buyer.
+            </p>
+          </>
+        ),
+      },
+      {
+        q: "Why must the owner/buyer bear the administrative costs for the perfection of transfer?",
+        a: () => (
+          <>
+            <p>
+              According to Section 287(1) of the Companies Act 1965, a
+              Liquidator is not obligated to incur any expenses related to the
+              company&apos;s winding up unless there are adequate assets
+              available. Therefore, the costs associated with administrative
+              procedures for the perfection of transfer must be covered by the
+              owner/buyer.
+            </p>
+          </>
+        ),
+      },
+    ],
+  },
+];
---
+++ lib/faqMalay.tsx
@@ -0,0 +1,257 @@
+export const Malay = [
+  {
+    "Prosedur Kebankrapan dan Proses Pelikuidasian": [
+      {
+        q: "Apakah Maksud Likuidasi Atau Syarikat Dalam Penggulungan?",
+        a: () => (
+          <p>
+            Likuidasi adalah proses di mana aset syarikat dijual atau diubah
+            menjadi wang untuk membayar hutang-hutang kepada pemiutang.
+          </p>
+        ),
+      },
+      {
+        q: "Bila Boleh Sesebuah Syarikat Digulungkan Oleh Mahkamah?",
+        a: () => (
+          <>
+            <p>Menurut Seksyen 218 Akta Syarikat 1965:</p>
+            <ul>
+              <li>
+                Syarikat membuat keputusan melalui resolusi khas untuk digulung
+                oleh mahkamah.
+              </li>
+              <li>Syarikat gagal membayar hutang.</li>
+              <li>
+                Pengarah-pengarah bertindak demi kepentingan peribadi dan tidak
+                adil kepada ahli-ahli syarikat
+              </li>
+              <li>
+                Mahkamah memutuskan adalah adil dan wajar untuk syarikat
+                digulungkan.
+              </li>
+              <li>
+                Bilangan ahli syarikat berkurang hingga tinggal seorang, kecuali
+                bagi syarikat Milikan Tunggal.
+              </li>
+              <li>
+                Syarikat tidak memulakan perniagaan dalam tempoh setahun selepas
+                pemerbadanan atau menghentikan perniagaan untuk tempoh yang
+                lama.
+              </li>
+              <li>
+                Syarikat telah mencapai tempoh penamatan yang ditetapkan dalam
+                memorandum & artikelnya, atau kejadian yang sepatutnya
+                menyebabkan penamatan telah berlaku.
+              </li>
+              <li>
+                Syarikat digunakan untuk tujuan yang menyalahi undang-undang
+                atau merugikan keselamatan negara atau kepentingan umum.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Siapa Yang Boleh Dilantik Sebagai Pelikuidasi?",
+        a: () => (
+          <>
+            <ul>
+              <li>Pegawai Penerima Insolvensi (DGI) atau</li>
+              <li>Pelikuidasi persendirian yang berlesen.</li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Apakah Tugas Utama Pelikuidasi?",
+        a: () => (
+          <ul>
+            <li>
+              Melikuidasi syarikat dengan mengagihkan aset kepada pemiutang dan,
+              jika ada baki, diagihkan kepada pemegang saham.
+            </li>
+            <li>
+              Kuasa pengarah syarikat diambil alih oleh pelikuidasi sebaik
+              sahaja proses pelikuidasi dimulakan.
+            </li>
+            <li>
+              Pelikuidasi bertanggungjawab memastikan proses pembubaran syarikat
+              dilakukan secara tuntas.
+            </li>
+          </ul>
+        ),
+      },
+      {
+        q: "Siapakah Pemiutang Yang Diasuransikan (Secured Creditor) Dan Yang Tidak Diasuransikan (Unsecured Creditor)?",
+        a: () => (
+          <>
+            <strong>Pemiutang Yang Diasuransikan (Secured Creditor)</strong>
+            <p>
+              Institusi atau korporasi yang memberikan pinjaman kepada syarikat
+              dengan hak undang-undang untuk menuntut aset syarikat sebagai
+              jaminan hutang. Pemiutang ini harus mengemukakan bukti hutang
+              kepada Pelikuidasi.
+            </p>
+            <strong>
+              Pemiutang Yang Tidak Diasuransikan (Unsecured Creditor)
+            </strong>
+            <p>
+              Pemiutang yang tidak memiliki jaminan aset syarikat harus juga
+              mengemukakan bukti hutang kepada Pelikuidasi.
+            </p>
+          </>
+        ),
+      },
+    ],
+    "Proses Pengesahan Hakmilik": [
+      {
+        q: "Tujuan Proses Pengesahan Hakmilik",
+        a: () => (
+          <>
+            <p>Proses ini penting untuk:</p>
+            <ul>
+              <li>
+                Memastikan syarikat menyimpan rekod lengkap pemilik dan dokumen
+                pendukung.
+              </li>
+              <li>
+                Mematuhi Seksyen 22D (3) Akta Perumahan (Kawalan dan
+                Perlesenan), 1966.
+              </li>
+              <li>Mempercepatkan proses penyerahan hakmilik strata.</li>
+              <li>
+                Menghindari kegiatan penipuan berkaitan dengan unit oleh pihak
+                ketiga.
+              </li>
+              <li>
+                Mengidentifikasi unit yang masih dimiliki oleh syarikat yang
+                dikelola.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Kepentingan Pengemukakan Bukti Pembelian/Pemilikan",
+        a: () => (
+          <>
+            <p>Mengemukakan dokumen bukti penting untuk:</p>
+            <ul>
+              <li>Mencegah penipuan oleh pihak ketiga.</li>
+              <li>Membuat rekod kepemilikan yang terperinci dan lengkap.</li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Akibat Tidak Mengemukakan Bukti Pembelian/Pemilikan",
+        a: () => (
+          <>
+            <p>Jika bukti tidak diberikan:</p>
+            <li>
+              Pelikuidasi akan meminta arahan mahkamah untuk mengambil alih unit
+              atau harta yang bersangkutan.
+            </li>
+          </>
+        ),
+      },
+      {
+        q: " Dokumen Pendukung Pengesahan Pemilikan",
+        a: () => (
+          <>
+            <p>Berikut ini adalah dokumen yang diperlukan:</p>
+            <ul>
+              <li>Salinan Perjanjian Jual Beli.</li>
+              <li>
+                Salinan bukti pembayaran lengkap atau surat penyerahan hak.
+              </li>
+              <li>
+                Untuk pembayaran pinjaman: Perjanjian Pinjaman Bank, Penyata
+                Bank, Surat Penerimaan dan Penyerahhakkan Balik.
+              </li>
+              <li>
+                Salinan Resit Taksiran untuk pembayaran cukai pintu terkini
+                kepada Pihak Berkuasa Tempatan.
+              </li>
+              <li>
+                Salinan bil penyelenggaraan terkini (3 bulan terakhir) untuk
+                pembangunan bertingkat.
+              </li>
+              <li>Dokumen lain yang relevan dan berkaitan.</li>
+            </ul>
+          </>
+        ),
+      },
+    ],
+    "Permohonan Pindah Milik": [
+      {
+        q: "Kepentingan Menyempurnakan Pindah Milik/Gadaian",
+        a: () => (
+          <>
+            <p>
+              Menyempurnakan pindah milik atau gadaian (perfection of
+              transfer/charge) adalah penting karena:
+            </p>
+            <ul>
+              <li>
+                Itu menunjukkan bukti sah kepemilikan untuk berbagai transaksi
+                seperti penjualan, gadai, atau pinjaman.
+              </li>
+              <li>
+                Sesuai dengan Seksyen 40A, Akta Hakmilik Strata (Pindaan) 2007,
+                pemilik baru harus menyelesaikan proses ini dalam 12 bulan
+                setelah pemberitahuan keluaran hakmilik strata oleh pemilik
+                asal.
+              </li>
+              <li>
+                Jika tidak, denda antara RM1,000 hingga RM10,000 per unit bisa
+                dikenakan.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Langkah-langkah Setelah Menerima Notis",
+        a: () => (
+          <>
+            <p>Setelah menerima notis, pemilik/pembeli harus:</p>
+            <ul>
+              <li>
+                Menunjuk seorang peguam untuk mengirim surat instruksi kepada
+                pelikuidasi.
+              </li>
+              <li>
+                Pelikuidasi akan menandatangani Borang 14A dan/atau Borang
+                Kebenaran Pindah Milik/Gadaian jika dokumen yang diperlukan dan
+                pembayaran biaya administratif diserahkan.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        q: "Alasan Pemilik/Pembeli Membayar Biaya Administratif",
+        a: () => (
+          <>
+            <p>
+              Biaya administratif untuk menyempurnakan pindah milik/gadaian
+              diperlukan karena:
+            </p>
+            <ul>
+              <li>
+                Seksyen 287 (1) Akta Syarikat 1965 menetapkan bahwa pelikuidasi
+                tidak bertanggung jawab atas biaya yang terkait dengan syarikat
+                setelah likuidasi, kecuali ada aset yang cukup.
+              </li>
+              <li>
+                Oleh karena itu, pemilik/pembeli harus menanggung biaya
+                administratif yang berkaitan dengan proses ini.
+              </li>
+            </ul>
+          </>
+        ),
+      },
+    ],
+  },
+];
