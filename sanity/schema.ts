c2b8df4dddbeac5babe584db35153d44312f4146
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-08 23:09:14 +0000

    Add new schema types for image with description and album

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,51 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+
+const imageWithDescription: SchemaTypeDefinition = {
+  name: "imageWithDescription",
+  title: "Image with Description",
+  type: "image",
+  fields: [
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+      options: {
+        isHighlighted: true, // `isHighlighted` makes this field stand out in the Sanity Studio
+      },
+    },
+    // ... you can add more fields here if needed
+  ],
+  options: {
+    hotspot: true, // Enables the hotspot feature for image crops
+  },
+};
+
+const album: SchemaTypeDefinition = {
+  name: "album",
+  title: "Album",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "date",
+      type: "date",
+      title: "Date of Album",
+      description: "When was this album created or the event occurred?",
+    },
+    {
+      name: "images",
+      type: "array",
+      title: "Images",
+      of: [{ type: "imageWithDescription" }],
+    },
+    // ... add other fields like author, description, etc.
+  ],
+};
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [album, imageWithDescription], // Add your types to this array
+};
