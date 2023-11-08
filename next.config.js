b323d6d72bd3b8e393ce75c67c27828674f89ceb
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-08 23:07:04 +0000

    Add remotePatterns to images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/yd0p4ozm/production/**/*",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
