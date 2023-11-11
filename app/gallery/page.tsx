5f22ba6b58b94bf7ffdcdf7a4cfc5485c3377151
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-11 00:21:19 +0000

    Refactor gallery page to use Albums component with Skeleton fallback

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,72 +1,15 @@
 "use client";
-// Use client for now but fetch on server and pass as prop to page
-import React from "react";
-import { useState } from "react";
-import { client } from "../../sanity/lib/client";
-import { urlForImage } from "@/sanity/lib/image";
-import Image from "next/image";
-
-// Define the Album type
-type Album = {
-  _id: string;
-  title: string;
-  date: string;
-  coverImage: {
-    asset: {
-      _ref: string;
-      url: string;
-    };
-  };
-};
+import React, { Suspense } from "react";
+import Albums from "@/components/ui/gallery/Albums";
+import { Skeleton } from "@/components/shadcn/ui/skeleton";
 
 const GalleryPage = () => {
-  const [albums, setAlbums] = useState<Album[]>([]);
-
-  // Fetch albums when component mounts
-  React.useEffect(() => {
-    const fetchAlbums = async () => {
-      const query = `
-      *[_type == "album"] {
-        _id,
-        title,
-        date,
-        "coverImage": coverImage {
-          asset-> {
-            _id,
-            url
-          }
-        }
-      }
-    `;
-      const results = await client.fetch<Album[]>(query);
-      setAlbums(results);
-      // console.log(albums[0].coverImage.asset.url);
-    };
-
-    fetchAlbums();
-  }, []);
-
   return (
     <div>
       <h1>Gallery</h1>
-      <ul>
-        {albums.map((album) => (
-          <li key={album._id} className="w-fit">
-            <a href={`/gallery/${album._id}`}>
-              {album.coverImage && (
-                <Image
-                  src={album.coverImage.asset.url}
-                  alt={`Cover for ${album.title}`}
-                  width={400} // Set your desired styles
-                  height={400} // Set your desired styles
-                  layout="responsive" // or fixed, intrinsic, etc., depending on your design needs
-                />
-              )}
-              {album.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<Skeleton className="h-44 w-44" />}>
+        <Albums />
+      </Suspense>
     </div>
   );
 };
