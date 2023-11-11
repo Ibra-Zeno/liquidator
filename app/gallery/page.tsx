e9f3d04d11d39be39cf6944e6c6c3d809cd6773b
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-11 00:58:46 +0000

    Refactor gallery page layout

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,16 +1,18 @@
-"use client";
+// "use client";
 import React, { Suspense } from "react";
 import Albums from "@/components/ui/gallery/Albums";
 import { Skeleton } from "@/components/shadcn/ui/skeleton";
 
 const GalleryPage = () => {
   return (
-    <div>
-      <h1>Gallery</h1>
-      <Suspense fallback={<Skeleton className="h-44 w-44" />}>
-        <Albums />
-      </Suspense>
-    </div>
+    <main className="">
+      <section className="mx-auto max-w-7xl py-16">
+        <h1>Gallery</h1>
+        <Suspense fallback={<Skeleton className="h-44 w-44" />}>
+          <Albums />
+        </Suspense>
+      </section>
+    </main>
   );
 };
 
