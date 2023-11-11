f2eceb1383717618215d10b31a6d03df57fe115e
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-11 01:28:12 +0000

    Update FAQ page layout and text

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -5,12 +5,14 @@ const FAQ: React.FC = () => {
   return (
     <>
       <section id="banner">
-        <div className="mx-auto grid max-w-2xl gap-y-8 py-12 text-center text-lg tracking-wide">
-          <span className=" font-semibold">Improve you asset management</span>
-          <h2 className="-mt-2 text-6xl font-bold">
+        <div className="mx-auto grid max-w-2xl py-12 text-center text-lg tracking-wide">
+          <span className="text-base font-semibold leading-7 text-indigo-600">
+            Improve you asset management
+          </span>
+          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Frequently Asked Questions
           </h2>
-          <p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
             Our mission is to provide a platform for all stakeholders in the
             property industry to collaborate and share information in a
             transparent manner.
