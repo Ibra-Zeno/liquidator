9bca48807b5624ad6a67b6f09d40d012664991d1
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-13 21:20:49 +0000

    Add logo with image and text to homepage.

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,12 +1,23 @@
-"use client";
 import { FC } from "react";
+import Image from "next/image";
 import Link from "next/link";
 interface LogoProps {}
 
 const Logo: FC<LogoProps> = ({}) => {
   return (
-    <Link href="/" className="p-1 text-3xl font-bold">
-      Logo
+    <Link href="/" className="text-3xl font-bold">
+      <div className="flex items-center">
+        <Image
+          src="/images/index/liquidatorLogo.png"
+          className=""
+          alt="theliquidator.net logo"
+          width={30}
+          height={30}
+        />
+        <h1 className="-mb-3 ml-3 font-logo text-lg tracking-wider">
+          theliquidator.net
+        </h1>
+      </div>
     </Link>
   );
 };
