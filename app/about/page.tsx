8461709e5a9ea3031bc96459c4bee6e331afa6fe
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-15 22:24:24 +0000

    Remove sticky content section from About page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,6 +20,7 @@ const About: FC<pageProps> = ({}) => {
         </div>
         <Breadcrumbs />
       </section>
+
       {/* Content (sticky) */}
       <section className="relative isolate overflow-hidden bg-white px-6 pt-12 lg:px-0">
         <div className="mx-auto grid max-w-3xl grid-cols-1 gap-x-40 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
