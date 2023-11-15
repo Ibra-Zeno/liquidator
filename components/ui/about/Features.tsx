ff1c374a54bf1c819b6f584b800324725a4e4f6f
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-11-15 20:28:38 +0000

    Update padding in Features section

--- components/ui/about/Features.tsx
+++ components/ui/about/Features.tsx
@@ -43,7 +43,7 @@ const values = [
 const Features: FC = ({}) => {
   return (
     <section className="mx-auto max-w-7xl">
-      <div className="container mx-auto px-6 py-10">
+      <div className="container mx-auto px-6 pt-20">
         <div className="lg:flex lg:items-center">
           <div className="w-full space-y-12 lg:w-1/2">
             <div>
