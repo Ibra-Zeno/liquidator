d73f303c4f0da4bdf651024bbb9075881cb2c455
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-09 22:15:47 +0000

    Refactor team member display in CategoryPage component

--- app/team/[slug]/page.tsx
+++ app/team/[slug]/page.tsx
@@ -50,7 +50,6 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   params: { slug: string };
 }) => {
   const teamMembers: PersonProps[] = await fetchMembers(params.slug as string);
-
   return (
     <main className="mx-auto flex max-w-7xl justify-between gap-x-6 px-6 pt-12">
       {/* <div className="w-fit max-w-md">
@@ -61,47 +60,67 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
       </div> */}
       <ul className="grid justify-between gap-x-4">
         {teamMembers.map((member) => (
-          <li key={member._id} className="mb-6 w-fit">
+          <li key={member._id} className="mb-6">
             {member.image ? (
-              <figure className="">
-                <Image
-                  src={member.image.asset.url}
-                  alt={member.name}
-                  width={400}
-                  height={100}
-                  className="mx-auto mb-2 aspect-auto h-[24rem] w-[24rem] rounded-sm object-cover object-top shadow-xl"
-                />
-                <figcaption className="mb-1.5 text-center text-xl font-semibold">
-                  {member.name}
-                </figcaption>
-              </figure>
+              <div className="grid grid-cols-5 gap-x-12">
+                <figure className="col-span-2">
+                  <Image
+                    src={member.image.asset.url}
+                    alt={member.name}
+                    width={1920}
+                    height={1400}
+                    className="mx-auto mb-2 aspect-auto max-h-[25rem] max-w-[20rem] rounded-sm object-cover object-top shadow-xl"
+                    style={{ width: "100%", height: "100%" }}
+                  />
+                </figure>
+                <div className="col-span-3 grid h-fit justify-between">
+                  <figcaption className="mb-2.5 text-3xl font-semibold">
+                    {member.name}
+                  </figcaption>
+                  <p className="mb-3 text-sm font-bold">{member.nickname}</p>
+                  {member.qualifications && (
+                    <div className="mb-6 flex items-center justify-start gap-x-3">
+                      <Sparkles
+                        strokeWidth={1.4}
+                        className="mr-2 rounded-full border border-gray-700/75 p-0.5"
+                      />
+                      <p className="inline-block text-center text-sm font-medium">
+                        {member.qualifications?.join(",  ")}
+                      </p>
+                    </div>
+                  )}
+                  <p className="mx-auto max-w-6xl  leading-7">
+                    {member.biography}
+                  </p>
+                </div>
+              </div>
             ) : (
               <h3 className="mb-3 text-center text-2xl font-semibold">
                 {member.name}
               </h3>
             )}
-            <p className="mb-3 text-center text-sm font-bold">
-              {member.nickname}
-            </p>
-            <div className="mx-auto mb-6 flex items-center justify-center gap-x-3">
-              <Sparkles
-                strokeWidth={1.4}
-                className="mr-2 rounded-full border border-gray-700/75 p-0.5"
-              />
-              {member.qualifications?.map((qualification, i) => (
-                <p
-                  key={i}
-                  className="inline-block max-w-sm text-center font-medium"
-                >
-                  {qualification}
-                </p>
-              ))}
-            </div>
-            <ScrollShadow className="mb-6">
+            {/* <div>
+              <p className="mb-3 text-center text-sm font-bold">
+                {member.nickname}
+              </p>
+              <div className="mx-auto mb-6 flex items-center justify-center gap-x-3">
+                <Sparkles
+                  strokeWidth={1.4}
+                  className="mr-2 rounded-full border border-gray-700/75 p-0.5"
+                />
+                {member.qualifications?.map((qualification, i) => (
+                  <p
+                    key={i}
+                    className="inline-block max-w-sm text-center font-medium"
+                  >
+                    {qualification}
+                  </p>
+                ))}
+              </div>
               <p className="mx-auto max-w-6xl px-4 text-center leading-7">
                 {member.biography}
               </p>
-            </ScrollShadow>
+            </div> */}
           </li>
         ))}
       </ul>
