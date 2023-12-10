a344dd1d20c844776137043d447a7bf410bfe292
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-10 22:33:22 +0000

    Refactor FAQAccordion component layout

--- components/ui/faq/FAQAccordion.tsx
+++ components/ui/faq/FAQAccordion.tsx
@@ -10,6 +10,7 @@ import {
 import { English } from "@/lib/faqEnglish";
 import { Malay } from "@/lib/faqMalay";
 import { Switch } from "@nextui-org/react";
+import { Separator } from "@/components/shadcn/ui/separator";
 
 const FAQAccordion = () => {
   const [isEnglish, setIsEnglish] = useState(true);
@@ -35,19 +36,28 @@ const FAQAccordion = () => {
           {data.map((section, index) => (
             <div key={index}>
               {Object.entries(section).map(([title, items], sectionIndex) => (
-                <div key={sectionIndex} className="py-8">
-                  <h2 className="pb-2 text-3xl font-semibold">{title}</h2>
-                  {items.map((item, itemIndex) => (
-                    <AccordionItem
-                      key={itemIndex}
-                      value={`item-${index}-${sectionIndex}-${itemIndex}`}
-                    >
-                      <AccordionTrigger>{item.q}</AccordionTrigger>
-                      <AccordionContent className="text-base leading-7">
-                        {item.a()}
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
+                <div
+                  key={sectionIndex}
+                  className="grid grid-cols-6 gap-x-4 px-6 py-8"
+                >
+                  <Separator
+                    className="col-span-6 mb-7"
+                    orientation="horizontal"
+                  />
+                  <h2 className="col-span-2 pt-4 font-bold">{title}</h2>
+                  <div className="col-span-4">
+                    {items.map((item, itemIndex) => (
+                      <AccordionItem
+                        key={itemIndex}
+                        value={`item-${index}-${sectionIndex}-${itemIndex}`}
+                      >
+                        <AccordionTrigger>{item.q}</AccordionTrigger>
+                        <AccordionContent className="text-base leading-7">
+                          {item.a()}
+                        </AccordionContent>
+                      </AccordionItem>
+                    ))}
+                  </div>
                 </div>
               ))}
             </div>
