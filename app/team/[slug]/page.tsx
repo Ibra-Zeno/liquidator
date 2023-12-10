54c3f2f4baa238159dcfac2cca11af1cd7eeebb0
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-10 20:43:29 +0000

    Update lucide-react import in team page

--- app/team/[slug]/page.tsx
+++ app/team/[slug]/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { client } from "../../../sanity/lib/client";
 import Image from "next/image";
 import { ScrollShadow } from "@nextui-org/react";
-import { Sparkles } from "lucide-react";
+import { GraduationCap } from "lucide-react";
 import TeamLinks from "@/components/ui/team/TeamLinks";
 
 export interface PersonProps {
@@ -53,18 +53,17 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 }) => {
   const teamMembers: PersonProps[] = await fetchMembers(params.slug as string);
   return (
-    <main className="mx-auto flex max-w-7xl flex-col justify-between gap-x-6 px-6 pt-12">
+    <main className="mx-auto flex max-w-7xl flex-col justify-between gap-x-6 px-6 pt-6">
+      <div
+        aria-hidden="true"
+        className="absolute -top-96 start-1/2 flex -translate-x-1/2 transform"
+      >
+        <div className="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl "></div>
+        <div className="rounded-fulls h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl "></div>
+      </div>
       <div className="relative overflow-hidden">
-        <div
-          aria-hidden="true"
-          className="absolute -top-96 start-1/2 flex -translate-x-1/2 transform"
-        >
-          <div className="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl "></div>
-          <div className="rounded-fulls h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl "></div>
-        </div>
-
         <div className="relative">
-          <div className="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-16">
+          <div className="mx-auto max-w-[85rem] px-4 py-6 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <p className="inline-block bg-gradient-to-l from-blue-600 to-violet-500 bg-clip-text text-sm font-medium text-transparent">
                 Who dunnit?
@@ -78,13 +77,16 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 
               <div className="mt-5 max-w-3xl">
                 <p className="text-lg text-gray-600 ">
-                  Preline UI is an open-source set of prebuilt UI components,
-                  ready-to-use examples and Figma design system based on the
-                  utility-first Tailwind CSS framework.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                  dolores ipsum aspernatur officia ex, soluta est laborum
+                  ducimus iusto dolorum aut reprehenderit veniam repellendus
+                  possimus vero adipisci ut.
                 </p>
               </div>
             </div>
-            <TeamLinks currentLink={params.slug} />
+            <div className="pb-4 pt-2.5">
+              <TeamLinks currentLink={params.slug} />
+            </div>
           </div>
         </div>
       </div>
@@ -110,9 +112,9 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                   <p className="mb-3 text-sm font-bold">{member.nickname}</p>
                   {member.qualifications && (
                     <div className="mb-6 flex items-center justify-start gap-x-3">
-                      <Sparkles
+                      <GraduationCap
                         strokeWidth={1.4}
-                        className="mr-2 rounded-full border border-gray-700/75 p-0.5"
+                        className="mr-2 rounded-full border border-[#f2bae8] p-0.5 shadow-sm shadow-[#0C7BB3]"
                       />
                       <p className="inline-block text-center text-sm font-medium">
                         {member.qualifications?.join(",  ")}
@@ -132,10 +134,10 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                   </figcaption>
                   <p className="mb-3 text-sm font-bold">{member.nickname}</p>
                   {member.qualifications && (
-                    <div className="mb-6 flex items-center justify-start gap-x-3">
-                      <Sparkles
+                    <div className="mb-6 flex items-center justify-start gap-x-3 pt-2">
+                      <GraduationCap
                         strokeWidth={1.4}
-                        className="mr-2 rounded-full border border-gray-700/75 p-0.5"
+                        className="mr-2 rounded-full border border-[#f2bae8] p-0.5 shadow-sm shadow-[#0C7BB3]"
                       />
                       <p className="inline-block text-center text-sm font-medium">
                         {member.qualifications?.join(",\u00a0  ")}
@@ -148,28 +150,6 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                 </div>
               </li>
             )}
-            {/* <div>
-              <p className="mb-3 text-center text-sm font-bold">
-                {member.nickname}
-              </p>
-              <div className="mx-auto mb-6 flex items-center justify-center gap-x-3">
-                <Sparkles
-                  strokeWidth={1.4}
-                  className="mr-2 rounded-full border border-gray-700/75 p-0.5"
-                />
-                {member.qualifications?.map((qualification, i) => (
-                  <p
-                    key={i}
-                    className="inline-block max-w-sm text-center font-medium"
-                  >
-                    {qualification}
-                  </p>
-                ))}
-              </div>
-              <p className="mx-auto max-w-6xl px-4 text-center leading-7">
-                {member.biography}
-              </p>
-            </div> */}
           </li>
         ))}
       </ul>
