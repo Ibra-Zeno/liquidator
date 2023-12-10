e53d0a4c46c49123325bf54a21226dbb19d8b5a1
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-10 00:16:47 +0000

    Refactor TeamLinks component to highlight current link

--- components/ui/team/TeamLinks.tsx
+++ components/ui/team/TeamLinks.tsx
@@ -1,4 +1,3 @@
-interface TeamLinksProps {}
 import Link from "next/link";
 import { Chip } from "@nextui-org/react";
 const teamLinks = [
@@ -32,11 +31,19 @@ const teamLinks = [
   },
 ];
 
-const TeamLinks: React.FC<TeamLinksProps> = ({}) => {
+const TeamLinks: React.FC<{ currentLink: string }> = ({ currentLink }) => {
+  const currentPath = "/team/" + currentLink;
   return (
-    <section className="my-6 flex justify-center gap-x-12 border-y border-y-gray-500/30 py-2.5">
+    <section className="my-6 flex justify-center gap-x-3 border-y border-y-gray-500/30 py-2.5">
       {teamLinks.map((link, i) => (
-        <Chip key={i}>
+        <Chip
+          key={i}
+          className={
+            currentPath === link.link
+              ? "text-primary-500 shadow-lg"
+              : "text-black"
+          }
+        >
           <Link
             href={link.link}
             className="font-semibold transition-colors duration-300 ease-in-out hover:text-primary-500"
