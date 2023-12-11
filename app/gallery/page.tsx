6e923938a70246858203825ae20f8a8d587d5b9c
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-11 21:38:42 +0000

    Update gallery page layout and content

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -7,9 +7,41 @@ const GalleryPage = () => {
   return (
     <main className="">
       <section className="mx-auto max-w-7xl py-16">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Gallery
-        </h2>
+        <section id="banner" className="border-b border-gray-700/25 pb-3">
+          <div
+            aria-hidden="true"
+            className="absolute -top-96 start-1/2 flex -translate-x-1/2 transform"
+          >
+            <div className="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl "></div>
+            <div className="rounded-fulls h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl "></div>
+          </div>
+          <div className="relative overflow-hidden">
+            <div className="relative">
+              <div className="mx-auto max-w-[85rem] px-4 py-6 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                  <p className="inline-block bg-gradient-to-l from-blue-600 to-violet-500 bg-clip-text text-sm font-medium text-transparent">
+                    View Our Adventures
+                  </p>
+
+                  <div className="mt-5 max-w-2xl">
+                    <h1 className="block text-xl font-semibold tracking-tight text-gray-800 md:text-3xl lg:text-4xl">
+                      Gallery
+                    </h1>
+                  </div>
+
+                  <div className="mt-5 max-w-3xl">
+                    <p className="text-lg text-gray-600 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsa dolores ipsum aspernatur officia ex, soluta est
+                      laborum ducimus iusto dolorum aut reprehenderit veniam
+                      repellendus possimus vero adipisci ut.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <Suspense fallback={<Skeleton className="h-44 w-44" />}>
           <Albums />
         </Suspense>
