4d9239b2bec73908fdfb7f1b7b91ace6d601bb44
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-12-24 00:14:36 +0000

    Update text color in About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -49,7 +49,7 @@ const About: FC<pageProps> = ({}) => {
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="lg:max-w-[34rem]">
-                <p className="text-sm font-semibold leading-7 text-indigo-600">
+                <p className="text-sm font-semibold leading-7 text-emerald-700">
                   Expert Corporate Recovery and Insolvency Services
                 </p>
                 <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
