d14114b82519d31b8b9d76581170c36b9ba4f890
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-04-01 16:29:41 +0100

    Update button variant styles

--- components/shadcn/ui/button.tsx
+++ components/shadcn/ui/button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium tracking-wide leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
