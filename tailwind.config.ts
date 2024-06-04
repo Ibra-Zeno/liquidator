c1b7108cea5b90ce606dace080e478feca6b0530
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-06-05 00:39:33 +0100

    chore: Update tailwind.config.ts to extend maxWidth with "8xl" size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ module.exports = {
       },
     },
     extend: {
+      maxWidth: {
+        "8xl": "90rem",
+      },
       fontFamily: {
         logo: ["SatisfactionPro", "serif"],
       },
