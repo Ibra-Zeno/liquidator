acd1f768a9205ede5517ca44e7905d658432e3a1
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-06-05 00:25:23 +0100

    Added info of Useful links

---
+++ public/images/imageData.ts
@@ -0,0 +1,54 @@
+const imageData = [
+  {
+    name: "The Malaysian Bar",
+    src: "/images/logos/bar.png",
+    link: "https://www.malaysianbar.org.my/",
+  },
+  {
+    name: "Department of Director General of Land and Mines (Federal)",
+    src: "/images/logos/jkptg.png",
+    link: "https://www.jkptg.gov.my/en/",
+  },
+  {
+    name: "Department of Surveys and Mapping Malaysia",
+    src: "/images/logos/jupem.png",
+    link: "https://www.jupem.gov.my/ms",
+  },
+  {
+    name: "Ministry of Housing and Local Government",
+    src: "/images/logos/kpkt.png",
+    link: "https://www.kpkt.gov.my/",
+  },
+  {
+    name: "Malaysia Department of Insolvency (MDI)",
+    src: "/images/logos/mdi.png",
+    link: "https://www.mdi.gov.my/",
+  },
+  {
+    name: "Malaysia Institute of Accountants",
+    src: "/images/logos/mia.png",
+    link: "https://mia.org.my/",
+  },
+  {
+    name: "Myeg Services Berhad",
+    src: "/images/logos/myeg.png",
+    link: "https://www.myeg.com.my/",
+  },
+  {
+    name: "Real Estate and Housing Developers Association Malaysia",
+    src: "/images/logos/rehda.png",
+    link: "https://rehda.com/",
+  },
+  {
+    name: "Companies Commission of Malaysia",
+    src: "/images/logos/ssm.png",
+    link: "https://www.ssm.com.my/Pages/Home.aspx",
+  },
+  {
+    name: "The Board of Valuers, Appraisers and Estate Agents Malaysia",
+    src: "/images/logos/valuers.png",
+    link: "https://lppeh.gov.my/WP2016/",
+  },
+];
+
+export default imageData;
