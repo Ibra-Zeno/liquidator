b1542ab8ca5e1ce723a71a899edddcc3a23bdc27
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-06-05 00:40:46 +0100

    refactor: Add link functionality to LogoCarousel images

--- components/ui/index/LogoCarousel.tsx
+++ components/ui/index/LogoCarousel.tsx
@@ -2,12 +2,13 @@
 import imageData from "../../../public/images/imageData";
 import { Tooltip } from "@nextui-org/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const LogoCarousel: React.FC = () => {
   return (
-    <section className="bg-lightBg overflow-y-hidden">
+    <section className="bg-lightBg overflow-hidden pt-6">
       <div className="group relative flex max-h-[100px] w-full flex-col justify-center overflow-x-hidden bg-transparent align-baseline">
-        <div className="group-hover:pause relative flex w-[300%] animate-[carousel_35s_linear_infinite] flex-row overflow-y-hidden sm:w-[200%] sm:animate-[carousel_35s_linear_infinite] lg:w-[150%] lg:animate-[carousel_45s_linear_infinite]">
+        <div className="group-hover:pause relative flex w-[300%] animate-[carousel_35s_linear_infinite] flex-row overflow-y-hidden sm:w-[200%] sm:animate-[carousel_35s_linear_infinite] lg:w-[150%] lg:animate-[carousel_50s_linear_infinite]">
           {imageData.map((object, index) => (
             <Tooltip
               key={index}
@@ -16,17 +17,22 @@ const LogoCarousel: React.FC = () => {
               offset={-20}
               className="rounded-lg border-2 border-gray-400/10 text-sm tracking-wide"
             >
-              <Image
-                className="relative mx-auto my-3 flex h-20 w-20 flex-row justify-center rounded-lg object-contain"
-                src={object.src}
-                width={100}
-                height={100}
-                alt={`${object.name} logo`}
-              />
+              <Link
+                href={object.link}
+                className="relative mx-auto flex h-20 w-20 flex-row justify-center rounded-lg"
+              >
+                <Image
+                  className=" object-contain"
+                  src={object.src}
+                  width={100}
+                  height={100}
+                  alt={`${object.name} logo`}
+                />
+              </Link>
             </Tooltip>
           ))}
         </div>
-        <div className="group-hover:pause absolute flex w-[300%] translate-x-[200%] animate-[carousel-reverse_35s_linear_infinite] flex-row items-center overflow-x-hidden sm:w-[200%] sm:translate-x-[100%] sm:animate-[carousel-reverse_35s_linear_infinite] lg:w-[150%] lg:translate-x-[50%] lg:animate-[carousel-reverse_45s_linear_infinite] ">
+        <div className="group-hover:pause absolute flex w-[300%] translate-x-[200%] animate-[carousel-reverse_35s_linear_infinite] flex-row items-center overflow-x-hidden sm:w-[200%] sm:translate-x-[100%] sm:animate-[carousel-reverse_35s_linear_infinite] lg:w-[150%] lg:translate-x-[50%] lg:animate-[carousel-reverse_50s_linear_infinite] ">
           {imageData.map((object, index) => (
             <Tooltip
               key={index}
@@ -35,14 +41,18 @@ const LogoCarousel: React.FC = () => {
               offset={-20}
               className=" rounded-lg border-2 border-gray-400/10 text-sm tracking-wide"
             >
-              <Image
-                className="relative mx-auto my-3 flex h-20 w-20 flex-row justify-center rounded-lg object-contain"
-                src={object.src}
-                width={100}
-                height={100}
-                alt={`${object.name} logo`}
-                key={index}
-              ></Image>
+              <Link
+                href={object.link}
+                className="relative mx-auto flex h-20 w-20 flex-row justify-center rounded-lg"
+              >
+                <Image
+                  className=" object-contain"
+                  src={object.src}
+                  width={100}
+                  height={100}
+                  alt={`${object.name} logo`}
+                />
+              </Link>
             </Tooltip>
           ))}
         </div>
