61b14d105455bfc8b057ed36eca5b3892666361b
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-11-20 20:33:43 +0000

    useless

--- components/ui/index/HeroTest.tsx
+++ components/ui/index/HeroTest.tsx
@@ -10,16 +10,16 @@ import LogoCarousel from "./LogoCarousel";
 const HeroTest: React.FC = () => {
   return (
     <main className="relative ">
-      <div className="my-10">
+      <div className="py-20">
         <div className=" px-6">
           <div className="grid  grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20  lg:grid-cols-3 ">
             <div className="col-span-2 mx-auto max-w-2xl lg:max-w-none">
               <div className="isolate z-30 col-span-2 w-full lg:pr-8 lg:pt-4">
                 <div className="relative isolate mx-auto -mt-20 flex h-[30rem] flex-col justify-center ">
-                  <h1 className="animate-fadeIn mb-8 font-serif text-4xl font-light italic leading-10 opacity-0 delay-100 fill-mode-forwards sm:text-5xl">
+                  <h1 className="mb-8 animate-fadeIn font-serif text-4xl font-light italic leading-10 opacity-0 delay-100 fill-mode-forwards sm:text-5xl">
                     Liquidation.
                   </h1>
-                  <h1 className="mb-20 animate-[fadeIn_ease-in_1s]  font-serif text-4xl font-light italic leading-10 opacity-0 delay-500 fill-mode-forwards sm:text-5xl">
+                  <h1 className="mb-20 animate-[fadeIn_ease-in_1s] text-4xl font-light italic leading-10 opacity-0 delay-500 fill-mode-forwards sm:text-5xl">
                     Expertise.
                   </h1>
                   <div className="mb-10 flex justify-start gap-x-6">
